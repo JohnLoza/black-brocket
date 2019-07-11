0010c4340bf45cb7c9ae0868d0a03868bbabf1b3
John Loza <lozabucio.jony@gmail.com>
2019-07-11 10:24:45 -0500

    add invoice folio form to local orders as well

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -418,7 +418,10 @@ class Admin::OrdersController < AdminController
   def save_invoice_folio
     deny_access! and return unless @current_user.has_permission?("orders@save_invoice_folio")
     order = Order.find_by!(hash_id: params[:id])
-    order.invoice_folio = params[:invoice_folio] and order.save!
+    unless order.invoice_folio.present?
+      order.invoice_folio = params[:invoice_folio]
+      order.save!
+    end
     flash[:success] = "Folio guardado"
 
     redirect_to admin_orders_path(type: params[:type])
